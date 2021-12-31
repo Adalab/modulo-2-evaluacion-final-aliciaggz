9f69732bde3e989ffbd86549788b91ceaa80f4ba
Alicia Gallego Gonzalez <aliciagallegonz@gmail.com>
2021-12-31 11:02:58 +0100

    add js and html

--- src/js/main.js
+++ src/js/main.js
@@ -4,11 +4,13 @@ const sectionAnime = document.querySelector('.js-section');
 const inputUser = document.querySelector('.js-input');
 const searchButton = document.querySelector('.js-btn-search');
 
-// const aside = document.querySelector('.js-section');
+const asideFavsElement = document.querySelector('.js-favanime');
 
-// let animeList = [];
+let animeList = [];
 
-// let favAnimeList = [];
+let favAnimeList = [];
+
+//get data from API
 
 const getApiFromSearch = () => {
   fetch(`https://api.jikan.moe/v3/search/anime?q=${inputUser.value}`)
@@ -17,7 +19,7 @@ const getApiFromSearch = () => {
       sectionAnime.innerHTML = '';
 
       const dataResult = data.results;
-      //   animeList = dataResult;
+      animeList = dataResult;
 
       for (let i = 0; i < dataResult.length; i++) {
         if (dataResult.image_url === undefined) {
@@ -27,9 +29,124 @@ const getApiFromSearch = () => {
           );
         }
       }
-
-      //   renderAll(animeList);
+      replaceImage();
+      renderAll(animeList);
     });
+};
+
+//image replace
+
+const replaceImage = () => {
+  for (let i = 0; i < animeList.length; i++) {
+    if (animeList.image_url === undefined) {
+      animeList[i].image_url = animeList[i].image_url.replace(
+        'https://cdn.myanimelist.net/images/qm_50.gif?s=e1ff92a46db617cb83bfc1e205aff620',
+        'https://via.placeholder.com/210x295/ffffff/666666/'
+      );
+    }
+  }
+};
+
+//paint elements
+
+const renderOne = (dataAnime) => {
+  sectionAnime.innerHTML += ` <article class="anime__article" data-id="${dataAnime.mal_id}">
+              <img class="anime-image" src="${dataAnime.image_url}" alt="image-of-anime">
+              <p class="title">${dataAnime.title}</p>
+          </article>`;
+};
+
+const renderAll = (dataAnime) => {
+  sectionAnime.innerHTML = '';
+
+  for (let index = 0; index < dataAnime.length; index++) {
+    renderOne(dataAnime[index]);
+  }
+  listenArticle();
+};
+
+//favorite section html
+const getFavAnimesCode = (favAnime) => {
+  let htmlCode = '';
+  htmlCode += `<article class="anime__article">
+  <img class="anime-image" src="${favAnime.image}" alt="image-of-anime">
+  <p class="title">${favAnime.title}</p>
+  <input class="js-btn-delete" data-id="${favAnime.id}" type="button" value="X">
+
+</article>`;
+  return htmlCode;
+};
+
+const paintFavAnimes = () => {
+  asideFavsElement.innerHTML = '';
+
+  for (const item of favAnimeList) {
+    asideFavsElement.innerHTML += getFavAnimesCode(item);
+  }
+  listenDeleteButtons();
+};
+
+function favAnimeStyle(event) {
+  const click = event.currentTarget;
+
+  click.classList.toggle('fav');
+  addFavToList(event);
+}
+
+function addFavToList(ev) {
+  const clickId = ev.currentTarget.dataset.id;
+  const clickIdParse = parseInt(clickId);
+
+  //comprobar si el anime esta en fav
+  const foundFav = favAnimeList.find((item) => item.id === clickIdParse);
+
+  if (foundFav === undefined) {
+    let foundAnime = animeList.find((item) => item.mal_id === clickIdParse);
+    // añado el anime favorito a la lista
+
+    favAnimeList.push({
+      id: foundAnime.mal_id,
+      title: foundAnime.title,
+      image: foundAnime.image_url,
+    });
+  }
+
+  //busca el anime clickado
+
+  // setLocalStorage();
+  paintFavAnimes();
+}
+
+//BONUS DELETE BUTTON
+function deleteFavAnime(event) {
+  const clickedBtn = event.currentTarget.dataset.id;
+  const clickBtnParse = parseInt(clickedBtn);
+
+  let foundIndex = favAnimeList.findIndex((item) => item.id === clickBtnParse);
+
+  favAnimeList.splice(foundIndex, 1);
+
+  paintFavAnimes();
+}
+//listeners
+
+function listenDeleteButtons() {
+  listenClickEvents('.js-btn-delete', deleteFavAnime);
+}
+
+function listenArticle() {
+  listenClickEvents('.anime__article', favAnimeStyle);
+}
+
+//LOCAL STORAGE
+
+//helper element
+
+const listenClickEvents = (selector, handler) => {
+  const elements = document.querySelectorAll(selector);
+  for (const element of elements) {
+    element.addEventListener('click', handler);
+  }
 };
 
 searchButton.addEventListener('click', getApiFromSearch);
